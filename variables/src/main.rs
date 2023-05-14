4a8c3582bc96433a3054fc4f7f611801ccd00f41
ness-leper <nmonforte@gmail.com>
2023-05-14 15:53:32 -0400

    Data Types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x:f64 = 5.05;
     println!("The value of x is: {x}");
@@ -12,6 +14,26 @@ fn main() {
 
     let (_x,y,_z) = tup;
     println!("The value of y is {y}");
+    println!("The third value of the tuple is {}",tup.2);
 
     println!("3 hrs: {THREE_HOURS_IN_SECONDS}");
+
+    let a: [i32;5] = [1,2,3,4,5];
+    println!("The array has {}", a[1]);
+
+    println!("Enter an array index:");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at {index} is: {element}");
 }
