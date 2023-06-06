481a54d9e9819c1f306df89fd2c85c5fea37be58
ness-leper <kuhazubat33azest@outlook.com>
2023-06-06 19:50:46 -0400

    Conditional branching

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let condition = false;
+    let number = if condition { 6 } else { 12 };
+
+    if number % 4 == 0 {
+        show_divide(4);
+    } else if number % 3 == 0 {
+        show_divide(3);
+    } else if number % 2 == 0 {
+        show_divide(2);
+    } else {
+        println!("number is not divisible by 4, 3, 2");
+    }
+}
+
+fn show_divide(res: i32) {
+    println!("number is divisible by {}", res);
 }
