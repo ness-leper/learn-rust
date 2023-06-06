940d7e185243bc908119185e7a44bdd622c008b8
ness-leper <kuhazubat33azest@outlook.com>
2023-06-06 19:39:03 -0400

    Fun with Functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,15 +1,25 @@
 fn main() {
     println!("Hello, world!");
-    
+
     another_function(6);
 
-    print_labeled_measurement(5,'h');
+    print_labeled_measurement(5, 'h');
+    println!("The value of x is: {}", five());
+    println!("The value of 6 + 1 is: {}",plus_one(6));
 }
 
-fn another_function(x: i32){
+fn another_function(x: i32) {
     println!("The value of x is {x}");
 }
 
-fn print_labeled_measurement(value:i32,unit_label:char){
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
